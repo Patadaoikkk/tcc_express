9cfb8367db30848e3d304c11b58554eb8fcc7734
patadaoikkk <delgm2020@gmail.com>
2024-06-16 17:12:58 -0300

    so falta arrumar a primeira rota e conectar com banco para testar

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('app', { title: 'Olá, mundo!' }); //tentando fazer funcionar
+  res.render('Principal pagina '); //tentando fazer funcionar
 });
 
 router.get('/psych', (req, res) => {
@@ -13,7 +13,7 @@ router.get('/user', (req, res) => {
   res.send('user cadastro'); //tentando fazer funcionar
 });
 router.get('/teste', (req, res) => {
-  res.send('Rota /teste funcionando'); //rotas teste nem eiste este arquivo
+  res.send('Rota teste funcionando'); //rotas teste nem eiste este arquivo
 });
 router.get('/index', (req, res) => {
   res.send('texto'); //outro teste 
