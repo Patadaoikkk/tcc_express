95a990e7c68bf2a91a9b13ee0c75994ea6ef2cd8
patadaoikkk <delgm2020@gmail.com>
2024-06-17 22:03:37 -0300

    arrumando as const de router e adicionar crud

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,9 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const psych = require('../models/psych');
+
 
 router.get('/', (req, res) => {
   res.render('Principal pagina '); //tentando fazer funcionar
